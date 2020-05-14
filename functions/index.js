1fe234c2bf26a64107d0f3409cdcc166312550c8
Emily <emily@sturman.org>
2020-05-13 23:14:11 -0700

    Some small tweaks to website, fixed firebase functionality w/ threads

--- functions/index.js
+++ functions/index.js
@@ -2,44 +2,53 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const sgMail = require("@sendgrid/mail");
 
-const MY_EMAIL = "emily@sturman.org";
-const FROM_EMAIL = "noreply.missfitsrobotics@gmail.com";
-const CONTACT_EMAIL_ID = "d-8d313d994f8f407ea698fe8ca0bedc23";
-const NEW_MEMBER_EMAIL_ID = "d-1744b4760fda43f684cff485cf2dbecb";
-
 admin.initializeApp();
 
-exports.sendTestEmail = () => {
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  sgMail.setSubstitutionWrappers("{{", "}}");
-  const msg = {
-    to: MY_EMAIL,
-    from: FROM_EMAIL,
-    templateId: CONTACT_EMAIL_ID,
-    substitutions: {
-      name: "Emily Sturman",
-      email: "emily@sturman.org",
-      comments: "This is a test. Can you see this?"
-    }
-  };
+function emailMessage(data, emailId, to) {
+  return {
+    to,
+    from: functions.config().emails.from,
+    replyTo: data.email,
+    templateId: emailId,
+    dynamicTemplateData: data,
+    substitutions: data
+  }
+}
 
+function sendEmail(data, emailId, to) {
+  const msg = emailMessage(data, emailId, to);
+  console.log(msg);
+  sgMail.setApiKey(functions.config()["send-grid"].key);
+  sgMail.setSubstitutionWrappers("{{", "}}");
   sgMail
     .send(msg)
-    .then(() => console.log("Contact email successfully sent."))
+    .then(() => console.log("Email successfully sent."))
     .catch((error) => {
       console.error(error);
       if (error.response) {
         console.error(error.response.body)
       }
     });
-};
+}
 
-exports.sendEmail = functions.firestore
+exports.sendApplicationEmail = functions.firestore
   .document("applications/{applicationId}")
   .onCreate((snap, context) => {
-    console.log("Executing function...");
+    console.log("Executing application function...");
     const data = snap.data();
     if (!data) {
       return;
     }
+    sendEmail(data, functions.config()["send-grid"]["new-member-email-id"], functions.config().emails["application-to"]);
+  });
+
+exports.sendContactEmail = functions.firestore
+  .document("contact/{contactId}")
+  .onCreate((snap, context) => {
+    console.log("Executing contact function...");
+    const data = snap.data();
+    if (!data) {
+      return;
+    }
+    sendEmail(data, functions.config()["send-grid"]["contact-email-id"], functions.config().emails["contact-to"]);
   });
